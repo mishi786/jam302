0900bc116ad107391035f8fe082843a4f7019673
mishi786 <68304964+mishi786@users.noreply.github.com>
2020-11-11 21:46:15 +0500

    Update jam.py

--- jam.py
+++ jam.py
@@ -282,7 +282,7 @@ def pilih_super():
 	
 			
 	def main(arg):
-		global cekpoint,oks
+		global oks
 		user = arg
 		try:
 			os.mkdir('out')
@@ -387,8 +387,8 @@ def pilih_super():
 	p.map(main, id)
 	print 42*"\033[1;96m="
 	print '\033[1;96m[\033[1;97m \033[1;96m] \033[1;92mProcess Has Been Completed \033[1;97m....'
-	print"\033[1;96m[+] \033[1;92mTotal OK\x1b[1;93mCP \033[1;91m: \033[1;92m"+str(len(oks))+"\033[1;97m/\033[1;93m"+str(len(cekpoint))
-	print("\033[1;96m[+] \033[1;92m THANKS FOR USING MY COMMAND ! WE WILL RIGHT BACK  \033[1;91m: \033[1;97mout/checkpoint.txt")
+	print"\033[1;96m[+] \033[1;92mTotal OK\033[1;91m: \033[1;92m"+str(len(oks))
+	print("\033[1;96m[+] \033[1;92m KING OF KING \033[1;91m: \033[1;97mout/checkpoint.txt")
 	raw_input("\n\033[1;96m[\033[1;97mBack\033[1;96m]")
 	menu()
 if __name__ == '__main__':
